d9fae64111b3b636c431cfdf3bd169cfd9484366
Elise Carter <elise.carter@cuanschutz.edu>
2022-04-18 01:29:07 -0600

    created error when curator file is open in excel

--- ProgramFiles/FindSessions.m
+++ ProgramFiles/FindSessions.m
@@ -1,5 +1,5 @@
 function [sessions,mouseDir] = FindSessions(CURdir)
-% goes into the Curator folder for each mouse and pull out session names
+% goes into the Curator folder for each mouse and pulls out session names
 
 mouseDir = dir(fullfile(CURdir.folder, CURdir.name));
 mouseFiles = {mouseDir.name};
@@ -7,5 +7,13 @@ mouseFiles = {mouseDir.name};
 % only excel files
 mouseDir = mouseDir(contains(mouseFiles,'.xlsx'));
 sessions = {mouseDir.name};
+
+openfiles = strfind(sessions,'~'); % "~" indicates file is open elsewhere
+if ~cellfun(@isempty,openfiles)
+    fileind = ~cellfun(@isempty,openfiles);
+    msg = "Curator file (" + sessions{fileind} + ") is open. Please close before reloading raw data.";
+    error(msg)
+    return
+end
 
 end
