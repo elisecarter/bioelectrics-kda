51a60c6bda7ea926bf5f13d48a6b1bd44f76f348
Elise Carter <elise.carter@cuanschutz.edu>
2022-07-18 15:31:14 -0600

    fixed check for dir in OutputData

--- private/OutputData.m
+++ private/OutputData.m
@@ -4,14 +4,14 @@ plot_folder = "Trajectory Plots";
 for i = 1:length(data)
     folder_name = data(i).MouseID;
     folder_path = fullfile(OUTpath,folder_name);
-%     if ~exists(folder_path)
-%         mkdir(folder_path)
-%     end
+     if ~exist(folder_path,'dir')
+         mkdir(folder_path)
+     end
     
     plot_path = fullfile(folder_path,plot_folder);
-%     if ~exists(plot_path)
-%         mkdir(plot_path)
-%     end
+     if ~exist(plot_path,'dir')
+         mkdir(plot_path)
+     end
 
     PlotTrajectories(data(i),plot_path)
     SaveJSON(data(i), folder_path)
