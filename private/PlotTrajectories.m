756a563b74ed3d477428eef86997d45e336b7b45
Elise Carter <elise.carter@cuanschutz.edu>
2022-07-28 13:36:41 -0600

    debugged plots going to kda window

--- private/PlotTrajectories.m
+++ private/PlotTrajectories.m
@@ -1,14 +1,15 @@
 function PlotTrajectories(mouse_data,path)
 
-figure("Visible","off") %change visibilty
+p = figure("Visible","off"); %create invisible figure
 
 for i = 1:length(mouse_data.Sessions) % num sessions for this mouse
+    p_ax = axes(p);
     session_data = mouse_data.Sessions(i);
     session_str = session_data.SessionID{1}; %this will break when I take session ID out of cell
     [avg_traj,x,y,~] = CalculateAvgTrajectory(session_data.InitialToMax);
-    plot(x,y,'Color','#918e8e')
+    plot(p_ax,x,y,'Color','#918e8e')
     hold on
-    plot(avg_traj(:,1),avg_traj(:,2),'LineWidth', 1.5 ,'Color','#000000')
+    plot(p_ax,avg_traj(:,1),avg_traj(:,2),'LineWidth', 1.5 ,'Color','#000000')
 
     xlabel('x (mm)')
     ylabel('y (mm)')
@@ -17,7 +18,7 @@ for i = 1:length(mouse_data.Sessions) % num sessions for this mouse
     str3 = sprintf('Session: %s',session_str); 
     title({str1;str2;str3},'Interpreter','none')
     
-    saveas(gcf,fullfile(path,session_str),'png')
+    saveas(p,fullfile(path,session_str),'png')
 
     clf
 end
