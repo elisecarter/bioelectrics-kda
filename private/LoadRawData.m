c32c63fb86b32727e520175952d70073139b8971
Elise Carter <elise.carter@cuanschutz.edu>
2022-08-29 21:06:51 -0600

    cleanup LoadRawData using dynamic fields

--- private/LoadRawData.m
+++ private/LoadRawData.m
@@ -3,39 +3,31 @@ function data = LoadRawData(data,MATpath,CURdir)
 
 f = waitbar(0,'Please wait...'); % create wait bar
 
-for i = 1:length(data) % i: mouse index
+for i = 1:length(data) % iterate thru mice
     waitstr = "Loading raw data... (" + data{i}.MouseID + ")";
     waitbar(i/length(data),f,waitstr);
 
     [sessionFiles,mouseDir] = FindSessions(CURdir(i));
     
-    raw_data = struct('Session',[],'ReachIndexPairs',[],...
-            'StimLogical',[],'pelletX_100',[],...
-            'pelletY_100',[],'pelletZ_100',[],'pelletConfXY_10k',[],...
-            'pelletConfZ_10k',[],'handX_100',[],'handY_100',[], ...
-            'handZ_100',[],'handConfXY_10k',[],'handConfZ_10k',[]);
-
     % find Matlab_3D files with names matching mouse curator files and load
-    for j = 1:length(sessionFiles) % j: session index
+    raw_data = struct;
+    for j = 1:length(sessionFiles) % iterate thru sessions
 
         fileStr = [sessionFiles{j}(1:27), '3D.mat'];
-        MATdata = load(fullfile(MATpath,fileStr)); % uint16 array
+        MATdata = load(fullfile(MATpath,fileStr),'table3D'); % uint16 array
         CURdata = readtable(fullfile(mouseDir(j).folder,mouseDir(j).name));
 
-        % convert uint16 data in table3D to double, store in rawData
+        fieldnames = MATdata.table3D.Properties.VariableNames;
+
         raw_data(j).Session = MATdata.table3D.Properties.RowNames;
         raw_data(j).ReachIndexPairs = CURdata(:,1:3);
         raw_data(j).StimLogical = logical(table2array(CURdata(:,4)));
-        raw_data(j).pelletX_100 = double(MATdata.table3D{1,1}{:,:});
-        raw_data(j).pelletY_100 = double(MATdata.table3D{1,2}{:,:});
-        raw_data(j).pelletZ_100 = double(MATdata.table3D{1,3}{:,:});
-        raw_data(j).pelletConfXY_10k = double(MATdata.table3D{1,4}{:,:});
-        raw_data(j).pelletConfZ_10k = double(MATdata.table3D{1,5}{:,:});
-        raw_data(j).handX_100 = double(MATdata.table3D{1,6}{:,:});
-        raw_data(j).handY_100 = flip(double(MATdata.table3D{1,7}{:,:}));
-        raw_data(j).handZ_100 = double(MATdata.table3D{1,8}{:,:});
-        raw_data(j).handConfXY_10k = double(MATdata.table3D{1,9}{:,:});
-        raw_data(j).handConfZ_10k = double(MATdata.table3D{1,10}{:,:});
+        
+        % convert uint16 data in table3D to double, store in rawData
+        for k = 1:length(fieldnames)-1 % everything expect crop pts
+            raw_data(j).(fieldnames{k}) = double(MATdata.table3D{1,k}{:,:});
+        end
+
     end
     data{i}.RawData = raw_data;
     data{i}.Status = 'Raw';
