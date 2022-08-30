5601d0f847afdf874d08a5e48bd91ce0c7f66f63
Elise Carter <elise.carter@cuanschutz.edu>
2022-08-29 21:07:17 -0600

    delete raw data after preprocessing reaches

--- private/PreprocessMice.m
+++ private/PreprocessMice.m
@@ -3,13 +3,15 @@ function data = PreprocessMice(data)
 %temp = [data{1:end}];
 %MouseIDs = {temp.MouseID};
 %[~,indx] = SelectMice(MouseIDs);
-newdata = data(indx);
+%newdata = data(indx);
 
 f = waitbar(0,'Please wait...'); % create wait bar
 for i = 1:length(data)
     waitstr = "Preprocessing raw data... (" + data{i}.MouseID + ")";
     waitbar(i/length(data),f,waitstr);
     data{i}.Sessions = PreprocessReachEvents(data{i}.RawData);
+    % raw data indexed at reaches & saved in previous step
+    data{i} = rmfield(data{i},'RawData');
     % index reach max here or in PRE ?
     data{i}.Status = 'Kinematics Extracted';
 end
