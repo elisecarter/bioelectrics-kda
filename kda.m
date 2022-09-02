085652660945f941bf7bff6e838f904f5bf79397
Elise Carter <elise.carter@cuanschutz.edu>
2022-09-02 09:45:03 -0600

    plotting session averages - still needs work

--- kda.m
+++ kda.m
@@ -26,7 +26,7 @@ window = figure( ...
 movegui(window,'center')
 
 % text to display on window
-opening_str = {'','Kinematic Data Analysis of Mouse Reach Events', ...
+opening_str = {'','Kinematic Data Analysis for use with CLARA', ...
     '', '', '', 'Navigate using Toolbar'};
 uicontrol(window, "Style", 'text', ...
     'String', opening_str, ...
@@ -41,15 +41,15 @@ uimenu(menu_file, 'Text', 'Save Session', 'Callback', @FileSaveSession)
 uimenu(menu_file, 'Text', 'Quit', 'Callback', @FileQuit)
 
 % analysis menu
-menu_process = uimenu(window, 'Label', 'Analysis');
-uimenu(menu_process, 'Text', 'Extract Kinematics', 'Callback', @AnalysisExtractKinematics)
-uimenu(menu_process, 'Text', 'Correlations', 'Callback', @AnalysisCorrelations,'Enable','off')
-uimenu(menu_process, 'Text', 'Compare Cohorts', 'Callback', @AnalysisCompareCohorts,'Enable','off')
+menu_analysis = uimenu(window, 'Label', 'Analysis');
+uimenu(menu_analysis, 'Text', 'Extract Kinematics', 'Callback', @AnalysisExtractKinematics)
+uimenu(menu_analysis, 'Text', 'Correlations', 'Callback', @AnalysisCorrelations,'Enable','off')
+uimenu(menu_analysis, 'Text', 'Compare Cohorts', 'Callback', @AnalysisCompareCohorts,'Enable','off')
 
-% % plotting menu
-% menu_plot = uimenu(window, 'Label', 'Plot');
-% uimenu(menu_plot, 'Text', 'Reach-to-Reach Averages', 'Callback', @PlotReach2Reach,'Enable','off')
-% uimenu(menu_plot, 'Text', 'Cohort Comparisons', 'Callback', @PlotCohortComp, 'Enable','off')
+% plot menu
+menu_plot = uimenu(window, 'Label', 'Plot');
+uimenu(menu_plot, 'Text', 'Session Means', 'Callback', @PlotSessionMeans,'Enable','off')
+
 
 % initialize data for nested functions
 data = [];
@@ -134,7 +134,7 @@ data = [];
             datacount = datacount+1;
             data{datacount} = load(fullfile(path,file),'-mat');
         end
-        
+
         DataSummary(data,window)
     end
 
@@ -145,6 +145,11 @@ data = [];
 
         % save data as .kda file (.mat file)
         path = uigetdir();
+        if path == 0
+            warning('User cancelled: No Output folder selected.')
+            return
+        end
+
         for i = 1:length(data)
             SaveKdaFile(data{i},path)
         end
@@ -164,10 +169,42 @@ data = [];
             warning('User cancelled: No Output folder selected.')
             return
         end
-
-        data = PreprocessMice(data);
+        
+        user_selections = UserSelections();
+        data = PreprocessMice(data, user_selections);
         DataSummary(data,window)
-        OutputData(data, OUTpath)
+        OutputData(data, OUTpath,user_selections)
+    end
+
+%% Plot Menu
+    function PlotSessionMeans(varargin)
+        quest = 'Would you like save session mean plots?';
+        dlgtitle = 'Session Plots Save Option';
+        btn1 = 'Yes';
+        btn2 = 'No';
+        defbtn = 'Yes';
+        answer = questdlg(quest,dlgtitle,btn1,btn2,defbtn);
+
+        if strcmpi(answer,btn1)
+            % user navigate to output directory
+            msg4 = msgbox('Navigate to Output Directory');
+            uiwait(msg4)
+            OUTpath = uigetdir();
+            if OUTpath == 0
+                warning('User cancelled: No Output folder selected.')
+                return
+            end
+        else
+            OUTpath = 0;
+        end
+
+        MouseIDs = [data{1:end}.MouseID];
+        %MouseIDs = {temp.MouseID};
+        [~,indx] = SelectMice(MouseIDs);
+        plotdata = data(indx);
+
+        PlotMeans(plotdata,OUTpath)
+
     end
 
 end
