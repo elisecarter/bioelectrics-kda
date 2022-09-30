a17e0ae069ae5f884d359a541f72e4c2c8d3f659
Elise Carter <elise.carter@cuanschutz.edu>
2022-09-29 22:13:47 -0600

    added switch in user selections for correlation coefficient selections

--- private/UserSelections.m
+++ private/UserSelections.m
@@ -1,29 +1,39 @@
-function user_selections = UserSelections()
+function user_selections = UserSelections(switch_exp)
 
-% Arc length? This takes a long time
-quest = 'Would you like to compute reach arc lengths? This increases computing time.';
-dlgtitle = 'Compute Arc Length Option';
-btn1 = 'Yes';
-btn2 = 'No';
-defbtn = 'Yes';
-answer = questdlg(quest,dlgtitle,btn1,btn2,defbtn);
+switch switch_exp
 
-if strcmpi(answer,btn1) % user selects yes
-    user_selections.ArcLength = 1;
-else
-    user_selections.ArcLength = 0;
-end
+    case 'ExtractKinematics'
+        % Arc length? This takes a long time
+        quest = 'Would you like to compute reach arc lengths? This increases computing time.';
+        dlgtitle = 'Compute Arc Length Option';
+        btn1 = 'Yes';
+        btn2 = 'No';
+        defbtn = 'Yes';
+        answer = questdlg(quest,dlgtitle,btn1,btn2,defbtn);
 
-% Save session trajectory plots?
-quest = 'Would you like save session trajectory plots?';
-dlgtitle = 'Trajectory Plot Save Option';
-btn1 = 'Yes';
-btn2 = 'No';
-defbtn = 'Yes';
-answer = questdlg(quest,dlgtitle,btn1,btn2,defbtn);
+        if strcmpi(answer,btn1) % user selects yes
+            user_selections.ArcLength = 1;
+        else
+            user_selections.ArcLength = 0;
+        end
 
-if strcmpi(answer,btn1) % user selects yes
-    user_selections.SavePlots = 1;
-else
-    user_selections.SavePlots = 0;
+        % Save session trajectory plots?
+        quest = 'Would you like save session trajectory plots?';
+        dlgtitle = 'Trajectory Plot Save Option';
+        btn1 = 'Yes';
+        btn2 = 'No';
+        defbtn = 'Yes';
+        answer = questdlg(quest,dlgtitle,btn1,btn2,defbtn);
+
+        if strcmpi(answer,btn1) % user selects yes
+            user_selections.SavePlots = 1;
+        else
+            user_selections.SavePlots = 0;
+        end
+
+    case 'Correlations'
+        list = {'','H2','H20'};
+
+        [indx,tf] = listdlg('ListString',list);
+
 end
