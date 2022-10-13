f2b160908956ed0401be0c4db1c4b674030851df
Elise Carter <elise.carter@cuanschutz.edu>
2022-10-12 23:35:47 -0600

    cleaned up avg trajectory function, no more CIs

--- private/AverageTrajectory.m
+++ private/AverageTrajectory.m
@@ -1,53 +1,22 @@
-function [avg_trajectory,stats] = AverageTrajectory(session_data)
+function [avg_trajectory,individual_trajectories] = AverageTrajectory(session_data)
 
-% pull out x, y, and z location with a column for each reach
 x = zeros(100,length(session_data));
 y = zeros(100,length(session_data));
 z = zeros(100,length(session_data));
+
+stacked_traj = [];
 for i = 1:length(session_data) % number of reaches
+    % store individual reaches in matrix for each dimension for plotting
     x(:,i) = session_data(i).InterpolatedHand(:,1);
     y(:,i) = session_data(i).InterpolatedHand(:,2);
     z(:,i) = session_data(i).InterpolatedHand(:,3);
+
+    % stack trajectories so we can take mean in the 3rd dimension
+    stacked_traj = cat(3,stacked_traj,session_data(i).InterpolatedHand);
 end
 
-% get average for each index (1:100) in each dimension
-mean_x = zeros(100,1);
-mean_y = zeros(100,1);
-mean_z = zeros(100,1);
-for i = 1:100
-    mean_x(i) = mean(x(i,:));
-    mean_y(i) = mean(y(i,:));
-    mean_z(i) = mean(z(i,:));
-end
+avg_trajectory = mean(stacked_traj,3);
 
-%calculate CIs for each index in each dimension
-CI_x = zeros(length(x),2);
-for i = 1:height(x)
-    standard_error = std(x(i,:))/sqrt(size(x,2)); %standard Error
-    ts = tinv([0.025  0.975],size(x,2)-1); %t-score for 95% conf. interval
-    CI_x(i,:) = mean_x(i) + ts.*standard_error; %confidence interval
-end
-
-CI_y = zeros(length(y),2);
-for i = 1:height(y)
-    standard_error = std(y(i,:))/sqrt(size(y,2)); 
-    ts = tinv([0.025  0.975],size(y,2)-1); 
-    CI_y(i,:) = mean_y(i) + ts.*standard_error;
-end
-
-CI_z = zeros(length(z),2);
-for i = 1:height(z)
-    standard_error = std(z(i,:))/sqrt(size(z,2)); 
-    ts = tinv([0.025  0.975],size(z,2)-1);
-    CI_z(i,:) = mean_z(i) + ts.*standard_error; 
-end
-
-avg_trajectory = [mean_x mean_y mean_z]; %euclidean
-
-% save stats for output
-stats.x = x;
-stats.y = y;
-stats.z = z;
-stats.CI_x = CI_x;
-stats.CI_y = CI_y;
-stats.CI_x = CI_z;
+individual_trajectories.x = x;
+individual_trajectories.y = y;
+individual_trajectories.z = z;
