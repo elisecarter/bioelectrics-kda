013f4d320baac12e557c22e485ba24f7a26599f3
Elise Carter <elise.carter@cuanschutz.edu>
2022-10-12 23:39:55 -0600

    added percent success and session level velocities

--- private/SessionMeans.m
+++ private/SessionMeans.m
@@ -2,13 +2,19 @@ function data = SessionMeans(data)
 % Adds session level mean velocity, distance to pellet (using max data),
 
 for i = 1:length(data.Sessions)
-    session_data = data.Sessions(i).InitialToMax; % interested in initial to max data
-
-    data.Sessions(i).MeanVelocity = SessionVelocity(session_data);
-    data.Sessions(i).MeanDistanceToPellet = ClosestDistanceToPellet(session_data);
+    session_data = data.Sessions(i); % interested in initial to max data
+    
+    % session level velocity
+    data.Sessions(i).MeanVelocity = SessionMeanVelocity(session_data.InitialToMax);
+    data.Sessions(i).MaxAbsVelocity = SessionMaxVelocity(session_data.InitialToMax);
+    %data.Sessions(i).MeanDistanceToPellet = ClosestDistanceToPellet(session_data.InitialToMax);
+    
+    % percent of successful reaches
+    num_success = sum(strcmp(session_data.Behavior,'success'));
+    num_reaches = length(session_data.Behavior);
+    data.Sessions(i).PercentSuccess = num_success/num_reaches;
 
     if exist('session_data.HandArcLength','var')
         data.Sessions(i).MeanArcLength = mean([session_data.HandArcLength]);
     end
-
 end
