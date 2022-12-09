540255d3b9d662411e7ea30c79c219ecba5e7793
Elise Carter <elise.carter@cuanschutz.edu>
2022-12-08 22:05:01 -0700

    corrected for new dropped frames columns in MATLAB 3D files

--- private/LoadRawData.m
+++ private/LoadRawData.m
@@ -26,7 +26,10 @@ for i = 1:length(data) % iterate thru mice
         raw_data(j).EndCategory = table2array(CURdata(:,5));
         
         % convert uint16 data in table3D to double, store in rawData
-        for k = 1:length(fieldnames)-1 % everything expect crop pts
+        for k = 1:length(fieldnames)-4 % dont need anything past cropPts in table
+%             if strcmpi(fieldnames{k},'cropPts') 
+%                 continue % dont need crop pts
+%             end
             raw_data(j).(fieldnames{k}) = double(MATdata.table3D{1,k}{:,:})';
         end
 
