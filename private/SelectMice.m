9e60c0a676cd73a447853c17ef0c1aeb5d324aa7
Elise Carter <elise.carter@cuanschutz.edu>
2022-12-20 15:30:25 -0700

    comments

--- private/SelectMice.m
+++ private/SelectMice.m
@@ -1,9 +1,9 @@
 function [mouseIDs,indx] = SelectMice(mouseIDs)
 % UI select mice
-
 prompt = 'Select Mice';
 [indx, ~] = listdlg('ListString',mouseIDs,'PromptString',prompt);
 
+% index out user selected mice
 mouseIDs = mouseIDs(indx);
 
 
