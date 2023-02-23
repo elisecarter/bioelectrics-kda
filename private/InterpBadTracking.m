c708594e42d7fdf809cb7f899a7fefe4347687d0
Elise Carter <elise.carter@cuanschutz.edu>
2023-02-23 13:54:18 -0700

    added functionality to interpolate thru poorly tracked points in the z-direction

--- private/InterpBadTracking.m
+++ private/InterpBadTracking.m
@@ -8,25 +8,35 @@ for i = 1:length(data.RawData) % iterate thru sessions
 
     xs = session.handX_100; %sample x vals
     ys = session.handY_100; %sample y vals
+    zs = session.handZ_100; %sample z vals
     frames = 1:length(xs); %sample frames
 
     % hand confidence vector for this session
-    conf = session.handConfXY_10k./10000; %remove the multiplier
-    lowConf = find(conf<.9); % poorly tracked = less than 90% confident
+    conf_XY = session.handConfXY_10k./10000; %remove the multiplier
+    conf_Z = session.handConfZ_10k./10000;
+
+    lowConf_XY = (conf_XY < .9); % poorly tracked = less than 90% confident
+    lowConf_Z = (conf_Z < .9);
 
     xq = frames; %query pts
-    x = frames; %sample pts
+    x_XY = frames; %sample pts for side camera
+    x_Z = frames; % sample pts for front camera
 
     % remove poorly tracked datapoints
-    x(lowConf) = []; 
-    xs(lowConf) = []; 
-    ys(lowConf) = []; 
+    x_XY(lowConf_XY) = []; 
+    x_Z(lowConf_Z) = [];
+
+    xs(lowConf_XY) = []; 
+    ys(lowConf_XY) = []; 
+    zs(lowConf_Z) = [];
     
     % interpolate
-    xsq = interp1(x,xs,xq,"pchip");
-    ysq = interp1(x,ys,xq,"pchip");
+    xsq = interp1(x_XY,xs,xq,"pchip");
+    ysq = interp1(x_XY,ys,xq,"pchip");
+    zsq = interp1(x_Z,zs,xq,"pchip");
     
     % store new vectors
     data.RawData(i).handX_100 = xsq';
     data.RawData(i).handY_100 = ysq';
+    data.RawData(i).handZ_100 = zsq';
 end
