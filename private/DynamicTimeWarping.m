a409b390109c118728874bb4505666951001ed24
Elise Carter <elise.carter@cuanschutz.edu>
2023-06-01 16:56:27 -0600

    added logic to index only moving frames in DTW and flag reaches that dtw cannot be calculated for

--- private/DynamicTimeWarping.m
+++ private/DynamicTimeWarping.m
@@ -1,4 +1,6 @@
-function DTW_euclidean = DynamicTimeWarping(euc)
+function [DTW_euclidean,deleteReach] = DynamicTimeWarping(euc)
+
+deleteReach = false;
 
 x = euc(:,1);
 y = euc(:,2);
@@ -8,12 +10,21 @@ try
     [pt,~,~] = interparc(linspace(0,1,100), x, y, z);
 
 catch % error in interparc due to position values staying same
-    moving_log = (diff(x)~=0 & diff(y)~=0 & diff(z)~=0);
-    x = x(moving_log);
-    y = y(moving_log);
-    z = z(moving_log);
-    [pt,~,~] = interparc(linspace(0,1,100), x, y, z);
-end
+     while any(diff(x)==0) || any(diff(y)==0) || any(diff(z)==0) %hand stays in same spot for more than 1 frame on any axis
+         is_moving = (diff(x)~=0 & diff(y)~=0 & diff(z)~=0);%index out only moving frames
+         x = x(is_moving); % index only the moving frames
+         y = y(is_moving);
+         z = z(is_moving);
+     end
+
+     if length(x) < 2
+         deleteReach = true;
+         DTW_euclidean = [];
+         return
+     end
+
+     [pt,~,~] = interparc(linspace(0,1,100), x, y, z);
+ end
 
 DTW_euclidean = pt;
 
