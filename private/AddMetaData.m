cade479d629e8fc99af3b209fd17f1d39cb41654
Elise Carter <elise.carter@cuanschutz.edu>
2023-07-17 10:07:43 -0600

    computing accuracy and failure type percentages based on raw data (pre filtering)

--- private/AddMetaData.m
+++ private/AddMetaData.m
@@ -1,27 +1,27 @@
 function data = AddMetaData(data)
 
 
-for i = 1:length(data.Sessions)
+for i = 1:length(data.RawData)
     % number of reach attemps
-    data.Sessions(i).ReachAttempts = length(data.Sessions(i).InitialToMax);
+    data.Sessions(i).ReachAttempts = length(data.RawData(i).Behaviors);
 
     % stimulation accuracy
-    stims = data.Sessions(i).StimLogical;
-    truepos = sum(strcmpi('success',data.Sessions(i).Behavior) & stims==1);
-    trueneg = sum(strcmpi('success',data.Sessions(i).Behavior)==0 & stims==0);
-    falsepos = sum(strcmpi('success',data.Sessions(i).Behavior)==0 & stims==1);
-    falseneg = sum(strcmpi('success',data.Sessions(i).Behavior) & stims==0);
+    stims = data.RawData(i).StimLogical;
+    truepos = sum(strcmpi('success',data.RawData(i).Behaviors) & stims==1);
+    trueneg = sum(strcmpi('success',data.RawData(i).Behaviors)==0 & stims==0);
+    falsepos = sum(strcmpi('success',data.RawData(i).Behaviors)==0 & stims==1);
+    falseneg = sum(strcmpi('success',data.RawData(i).Behaviors) & stims==0);
 
     data.Sessions(i).StimAccuracy = (truepos+trueneg)/(truepos+trueneg+falsepos+falseneg);
     data.Sessions(i).StimSensitivty = truepos/(truepos+falseneg);
     data.Sessions(i).StimSpecificity = trueneg/(trueneg+falsepos);
 
     % percentages of failure types
-    numfails = sum(contains(data.Sessions(i).Behavior,'fail'));
+    numfails = sum(contains(data.RawData(i).Behaviors,'fail'));
 
-    data.Sessions(i).PercentFailuresGrasp = sum(contains(data.Sessions(i).Behavior,'grasp'))/numfails;
-    data.Sessions(i).PercentFailuresReach = sum(contains(data.Sessions(i).Behavior,'reach'))/numfails;
-    data.Sessions(i).PercentFailuresRetrieval = sum(contains(data.Sessions(i).Behavior,'retrieval'))/numfails;
+    data.Sessions(i).PercentFailuresGrasp = sum(contains(data.RawData(i).Behaviors,'grasp'))/numfails;
+    data.Sessions(i).PercentFailuresReach = sum(contains(data.RawData(i).Behaviors,'reach'))/numfails;
+    data.Sessions(i).PercentFailuresRetrieval = sum(contains(data.RawData(i).Behaviors,'retrieval'))/numfails;
 
 end
 
