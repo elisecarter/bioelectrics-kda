cfc806dc5767090b984774c24980b8926b2c98ca
Elise Carter <elise.carter@cuanschutz.edu>
2023-07-21 13:56:11 -0600

    now using DTWHand to calculate expert reach

--- private/CalculateExpertReach.m
+++ private/CalculateExpertReach.m
@@ -4,14 +4,26 @@ function [data] = CalculateExpertReach(data)
 
 % pull out successful reach indices on last two days of training
 success_1 = strcmp(data.Sessions(end).Behavior,'success');
+success_data = data.Sessions(end).InitialToMax(success_1);
 
-% calculate the average trajectory of all successes on last two days of training
-[expert_1 ,~] = AverageTrajectory(data.Sessions(end).InitialToMax(success_1));
+stacked_traj = [];
+for i = 1:length(success_data) % number of reaches
+    % stack trajectories so we can take mean in the 3rd dimension
+    stacked_traj = cat(3,stacked_traj,success_data(i).DTWHand);
+end
+expert_1 = mean(stacked_traj,3);
 
 % check that there are at least two days of training to calculate expert traj. from
 if length(data.Sessions) > 1
     success_2 = strcmp(data.Sessions(end-1).Behavior,'success');
-    [expert_2 ,~] = AverageTrajectory(data.Sessions(end-1).InitialToMax(success_2));
+    success_data = data.Sessions(end-1).InitialToMax(success_2);
+
+    stacked_traj = [];
+    for i = 1:length(success_data) % number of reaches
+        % stack trajectories so we can take mean in the 3rd dimension
+        stacked_traj = cat(3,stacked_traj,success_data(i).DTWHand);
+    end
+    expert_2 = mean(stacked_traj,3);
     expert_stacked = cat(3,expert_1,expert_2);
 else
     expert_stacked = expert_1;
