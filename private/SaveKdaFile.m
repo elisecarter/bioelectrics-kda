a0048e4ba62256d496265d8aa4d3478218d57558
Elise Carter <elise.carter@cuanschutz.edu>
2024-03-11 10:42:26 -0600

    changed matlab file save to v7.3 or .h5

--- private/SaveKdaFile.m
+++ private/SaveKdaFile.m
@@ -12,4 +12,4 @@ end
 % save data as .kda file (.mat file)
 filename = sprintf('%s_%s_%s_%s.kda',data.Experimentor,data.MouseID,data.Phase,data.Status);
 file = fullfile(kda_path,filename);
-save(file,'-struct','data','-mat')
+save(file,'-struct','data','-v7.3','-mat')
