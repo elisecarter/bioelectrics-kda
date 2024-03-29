d71a4b9367651baf1f061a2c2eccf3a98732a75c
Elise Carter <elise.carter@cuanschutz.edu>
2024-03-29 09:56:49 -0600

    added specificaity when calculating means

--- private/SessionMeanVelocity.m
+++ private/SessionMeanVelocity.m
@@ -5,11 +5,11 @@ eucVelocities = zeros(length(session_data),3);
 absVelocities = zeros(length(session_data),1);
 maxVelocities = zeros(length(session_data),1);
 for i = 1:length(session_data)
-    eucVelocities(i,:) = mean(session_data(i).InterpolatedVelocity);
+    eucVelocities(i,:) = mean(session_data(i).InterpolatedVelocity,1);
     absVelocities(i) = mean(session_data(i).AbsoluteVelocity);
     maxVelocities(i) = session_data(i).MaxAbsVelocity;
 end
 
-meanEucVelocity = mean(eucVelocities);
+meanEucVelocity = mean(eucVelocities,1);
 meanAbsVelocity = mean(absVelocities);
 meanMaxVelocity = mean(maxVelocities);
